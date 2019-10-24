16e5a338dd907d64b4ce1ebaa7dd9888d5d0fb33
Michael Buffone <michaelbuffonee@gmail.com>
2019-10-23 23:57:31 -0400

    +

---
+++ Assignments/Assignment 4/QuestionA.asm
@@ -0,0 +1,32 @@
+TITLE QuestionA.asm
+;***************************************************************
+; -Michael Buffone
+; -October 11th, 2019
+; -COSC2406A19F Assignment 3, Question A
+
+; -This program will read an integer, store it into a DWORD, 
+;  and compute operations based on that input
+;***************************************************************
+INCLUDE Irvine32.inc
+
+.data
+pPrompt BYTE "You're kinda cute ngl", 0
+;----------Main Code Section------------------------------------
+.code
+main PROC
+	mov edx, OFFSET pPrompt						; Print msg and collect pVar
+	call WriteString
+main ENDP
+
+;***************************************************************
+; printArray PROC
+;	RECEIVES: ESI = offset of the source array
+;			  EBX = type of the source
+;			  ECX = number of elements in the source
+;***************************************************************
+;.data
+
+;.code
+
+;printArray ENP
+END main
