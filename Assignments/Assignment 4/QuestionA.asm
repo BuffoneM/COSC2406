744f27b1ebf0d4b4a0c165e4e376f366481c772a
Michael Buffone <BuffoneM@users.noreply.github.com>
2019-10-24 16:06:58 -0400

    +

--- Assignments/Assignment 4/QuestionA.asm
+++ Assignments/Assignment 4/QuestionA.asm
@@ -37,7 +37,7 @@ main PROC
 
 ;	****** Open the file ******
 	call OpenInputFile
-	mov fileHandle, eax
+	mov fileHandle, eax								; Save the file handle
 
 ;	****** Read from the file and store into letters array ******
 	mov edx, OFFSET letters							; Move the location of the letters array to edx
@@ -50,16 +50,23 @@ main PROC
 	mov ecx, LENGTHOF index							; Move ecx (loop) for the length of index array
 	call ReadFromFile
 
+;	****** Close the file ******
+	mov eax, fileHandle
+	call CloseFile
+
+;	*** Print the array ***
 	mov esi, OFFSET letters
 	mov ebx, TYPE letters
 	mov ecx, LENGTHOF letters
 	call PrintArrayChar 
-	call CrlF
 
 	mov esi, OFFSET index
 	mov ebx, TYPE index
 	mov ecx, LENGTHOF index
 	call PrintArrayInt 
+	
+
+;	******
 	exit
 main ENDP
 
@@ -105,14 +112,14 @@ printArrayInt PROC USES ecx ebx esi
 	dec ecx								; decrease the loop amount of ecx by one so you can print the last elements without a comma
 
 L2:
-	mov ax, [esi]						; print the next value of the array into ax
+	movzx eax, BYTE PTR [esi]			; print the next value of the array into ax
 	call WriteInt						
 	mov edx, OFFSET comma				; write the comma and space for the next value
 	call WriteString
 	add esi, ebx						; go to the next element
 	loop L2								; end of the loop
 	
-	mov ax, [esi]						; print the last value of the array into ax
+	movzx eax, BYTE PTR [esi]			; print the last value of the array into ax
 	call WriteInt
 	mov al, ']'							; print the last bracket and end the print function
 	call WriteChar
